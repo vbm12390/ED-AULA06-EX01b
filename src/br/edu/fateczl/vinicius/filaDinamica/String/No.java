591bf0267d6afef7fe677ba068eb7ea48435d9d5
vinicius <vinicius@DESKTOP-BKMT8RP>
2021-09-28 00:18:27 -0300

    Primeiro Commit

---
+++ src/br/edu/fateczl/vinicius/filaDinamica/String/No.java
@@ -0,0 +1,13 @@
+package br.edu.fateczl.vinicius.filaDinamica.String;
+
+public class No {
+
+	String dado;
+	No proximo;
+	
+	@Override
+	public String toString() {
+		return "Dado: "+dado;
+	}
+	
+}
